fe7bdcd1ea389af43e0b3c6b2ce8032baa5279f2
Aravindambalavanan <j_aravind@outlook.com>
2023-11-07 11:49:30 +0530

    Initial commit

---
+++ Java/DuplicatesArray.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+import java.util.Map.Entry;
+
+class GFG {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int t = sc.nextInt();
+    while (t-- > 0) {
+      int n = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < n; i++) a[i] = sc.nextInt();
+      Solution g = new Solution();
+      ArrayList<Integer> ans = g.duplicates(a, n);
+      for (Integer val : ans) System.out.print(val + " ");
+      System.out.println();
+    }
+  }
+}
+
+class Solution {
+
+  public static ArrayList<Integer> duplicates(int arr[], int n) {
+    int[] freq = new int[n];
+    ArrayList<Integer> list = new ArrayList<>();
+
+    for (int i = 0; i < n; i++) {
+      freq[arr[i]]++;
+    }
+    for (int i = 0; i < n; i++) {
+      if (freq[i] > 1) {
+        list.add(i);
+      }
+    }
+    if (list.isEmpty()) {
+      list.add(-1);
+    }
+    return list;
+  }
+}
