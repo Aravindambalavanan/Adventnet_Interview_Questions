fe01556b7e9e359df644bcec1fe3317a0c3e1599
Siva Prakash <prakashdeveloper@outlook.in>
2023-11-10 07:14:10 +0530

    Added & formatted files

--- Easy/Palindrome String/palindrome.cpp
+++ Easy/Palindrome String/palindrome.cpp
@@ -1,25 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-class Solution {
- public:
-  int isPalindrome(string S) {
-    string reversed = S;
-    reverse(S.begin(), S.end());
-    return (S == reversed) ? 1 : 0;
-  }
+class Solution
+{
+  public:
+    int isPalindrome(string S)
+    {
+        string reversed = S;
+        reverse(S.begin(), S.end());
+        return (S == reversed) ? 1 : 0;
+    }
 };
 
-int main() {
-  ios_base::sync_with_stdio(0);
-  cin.tie(NULL);
-  cout.tie(NULL);
-  int t;
-  cin >> t;
-  while (t--) {
-    string s;
-    cin >> s;
-    Solution ob;
-    cout << ob.isPalindrome(s) << "\n";
-  }
-  return 0;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+        Solution ob;
+        cout << ob.isPalindrome(s) << "\n";
+    }
+    return 0;
 }
--- Hard/Return two prime numbers/returnPrime.cpp
+++ Hard/Return two prime numbers/returnPrime.cpp
@@ -1,41 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
-class Solution {
- private:
-  bool isPrime(int k) {
-    for (int i = 2; i <= sqrt(k); i++) {
-      if (k % i == 0) {
-        return false;
-      }
-    }
-    return true;
-  }
-
- public:
-  vector<int> primeDivision(int N) {
-    vector<int> v;
-    for (int i = 2; i <= N; i++) {
-      if (isPrime(i)) {
-        if (isPrime(N - i)) {
-          v.push_back(i);
-          v.push_back(N - i);
-          break;
+class Solution
+{
+  private:
+    bool isPrime(int k)
+    {
+        for (int i = 2; i <= sqrt(k); i++)
+        {
+            if (k % i == 0)
+            {
+                return false;
+            }
         }
-      }
+        return true;
     }
-    return v;
-  }
+
+  public:
+    vector<int> primeDivision(int N)
+    {
+        vector<int> v;
+        for (int i = 2; i <= N; i++)
+        {
+            if (isPrime(i))
+            {
+                if (isPrime(N - i))
+                {
+                    v.push_back(i);
+                    v.push_back(N - i);
+                    break;
+                }
+            }
+        }
+        return v;
+    }
 };
 
-int main() {
-  int t;
-  cin >> t;
-  while (t--) {
-    int N;
-    cin >> N;
-    Solution ob;
-    vector<int> ans = ob.primeDivision(N);
-    cout << ans[0] << " " << ans[1] << "\n";
-  }
-  return 0;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin >> N;
+        Solution ob;
+        vector<int> ans = ob.primeDivision(N);
+        cout << ans[0] << " " << ans[1] << "\n";
+    }
+    return 0;
 }
